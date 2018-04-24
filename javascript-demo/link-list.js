46908836e8e05c89ddcc4d16340c98574a82710c
zhoujingzhi <604189994@qq.com>
2018-04-24 19:52:52 +0800

    链表-从链表中移除元素

--- javascript-demo/link-list.js
+++ javascript-demo/link-list.js
@@ -1,6 +1,6 @@
 "use strict";
-function LinkedList(){
-    let Node = function(item){
+function LinkedList() {
+    let Node = function (item) {
         this.item = item;
         this.next = null;
     }
@@ -10,32 +10,53 @@ function LinkedList(){
      * 在链表尾部追加元素
      * @param {*} item 
      */
-    this.append = function(item){
+    this.append = function (item) {
         let node = new Node(item);
         let current = null;
-        if(head === null){//如果第一个节点为空,将这个节点作为第一个节点 
+        if (head === null) {//如果第一个节点为空,将这个节点作为第一个节点 
             head = node;
-        }else{//如果第一个节点不为空
+        } else {//如果第一个节点不为空
             current = head;
             //循环访问列表
             //只要还有下一项就继续
             //直到找到最后一项目
-            while(current.next){
+            while (current.next) {
                 current = current.next;
             };
             current.next = node;
         };
         length++;//更新链表长度
     };
-    this.getHead = function(){
+    this.getHead = function () {
         return head
     };
-    this.insert = function(position,item){
-            
+    this.insert = function (position, item) {
+
     };
+    /**
+     * 删除目标元素的索引
+     * @param {*} index 
+     */
+    this.removeAt = function (index) {
+        if (typeof index != 'number' || index % 1 != 0 && index < 0 || index > length) return head;
+        let i = 0,
+            current = head,//目标
+            previous;//目标的上一个元素
+        if (index == 0) { //如果是删除第一个
+            head = current.next//直接把它的下一个元素变成它
+        } else {
+            //否则找到目标元素的上一个元素
+            while (i++ < index) {
+                previous = current;//目标的上一个元素
+                current = current.next//目标
+            }
+            previous.next = current.next;//将目标上一个元素的next指向目标下一个元素
+        };
+        return head
+    }
 };
 var linkedList = new LinkedList();
 linkedList.append('box');
 linkedList.append('box1');
 linkedList.append('box2');
-console.log(linkedList.getHead())
+console.log(linkedList.getHead());
--- jsplumb/jsplumb-demo/js/data.js
+++ jsplumb/jsplumb-demo/js/data.js
@@ -1,124 +1,8 @@
-var initialData1 = {
-    "Nodes":[
-        {
-        "Guid": "8fcbebd4-7f85-4043-acc0-05859f8dece9",
-        "CreateUser": "0003                          ",
-        "CreateDateTime": "/Date(1456882860000+0800)/",
-        "LastUpdateUser": "0003                          ",
-        "LastUpdateDateTime": "/Date(1456882860000+0800)/",
-        "Code": "BranchEnd8",
-        "NodeType": 2,
-        "NodeMode": 1,
-        "GroupGuid": "00000000-0000-0000-0000-000000000000",
-        "SubMode": 0,
-        "OrderNo": 0,
-        "Description": "",
-        "Version": 0,
-        "JumpMode": 0,
-        "RemarkMode": 0,
-        "AllowCustomExecutor": false,
-        "AllowCustomCC": false,
-        "AllowNextExecutor": false,
-        "NextExecutorIsRequired": false,
-        "CurrExecutorIsRequired": false,
-        "ExecutorWhere1": 0,
-        "ExecutorWhere2": 0,
-        "AllowCustomCCEmail": false,
-        "AllowRevoke": false,
-        "AllowBatchCancel": false,
-        "AllowBatchOK": false,
-        "AllowPriority": false,
-        "AllowCustomRollback": false,
-        "AllowNextCC": false,
-        "NextCCIsRequired": false,
-        "CustomCCIsRequired": false,
-        "Style": "left: 0px; top: 0px;",
-        "Main": {
-            "Guid": "a6b3bb43-99bb-4aa2-b51a-00e062d70531",
-            "CreateUser": "167                           ",
-            "CreateDateTime": "/Date(1398085122000+0800)/",
-            "LastUpdateUser": "0003                          ",
-            "LastUpdateDateTime": "/Date(1476005103000+0800)/",
-            "Code": "CopyTest",
-            "ViewNamePrefix": null,
-            "MainTable": null,
-            "SubFlow": false,
-            "Internal": null,
-            "Priority": 3,
-            "Category": "Performance",
-            "RunMode": 0,
-            "Enable": true,
-            "BatchApproval": false,
-            "BatchRefuse": true,
-            "AttachementType": 1,
-            "Version": null,
-            "TimeOut": null,
-            "AgencyMode": null,
-            "Description": "test",
-            "DialogWidth": null,
-            "DialogHeight": null,
-            "IconFileName": null,
-            "LargeIconFileName": null,
-            "RollbackFlowGuid": "00000000-0000-0000-0000-000000000000",
-            "UpdateFlowGuid": null,
-            "ExtendProperties": null,
-            "HasAttachement": true,
-            "UseFlowPrint": false,
-            "CommitNotAlertSuccess": false,
-            "GoBackNotAlertSuccess": false,
-            "MenuID": "aa",
-            "BeginNodeIsAutoPost": false,
-            "IsRepeatLaunch": false,
-            "RepeatLaunchTimeInterval": 0,
-            "OtherJson": "{\"CloseFlowEmail\": \"7e96b211-8050-422b-85cb-594f0bba8b8e\",\"NoApprovedEmail\":\"1fa86ea4-498f-44a1-ba6f-8d60048943d1\",\"AppUrl\":\"Leave/AppLeave/Application\",\"AddAppUrl\":\"Leave/AppLeave/Application4\",\"IsCalculateApprover\":\"0\",\"PostRemarkRequired\":\"0\",\"GoBackRemarkRequired\":\"0\",\"RefusedRemarkRequired\":\"0\",\"NotQuery\":\"0\",\"CustomFormXMLPath\":\"\",\"ShowOneStaffView\":\"0\"}",
-            "RevokeType": 0,
-            "Name": {
-                "English": "CopyTest",
-                "Chinese": "CopyTest",
-                "Language1": "CopyTest",
-                "Language2": "CopyTest",
-                "Language3": "CopyTest"
-            },
-            "Other": {
-                "CloseFlowEmail": "7e96b211-8050-422b-85cb-594f0bba8b8e",
-                "NoApprovedEmail": "1fa86ea4-498f-44a1-ba6f-8d60048943d1",
-                "AppUrl": "Leave/AppLeave/Application",
-                "AddAppUrl": "Leave/AppLeave/Application4",
-                "IsCalculateApprover": "0",
-                "PostRemarkRequired": "0",
-                "GoBackRemarkRequired": "0",
-                "RefusedRemarkRequired": "0",
-                "NotQuery": "0",
-                "CustomFormXMLPath": "",
-                "ShowOneStaffView": "0"
-            },
-            "ShouldLog": true
-        },
-        "Name": {
-            "English": "BranchEnd8",
-            "Chinese": "分支结束8",
-            "Language1": "",
-            "Language2": "",
-            "Language3": ""
-        },
-        "RemarkLabel": {
-            "English": "",
-            "Chinese": "",
-            "Language1": "",
-            "Language2": "",
-            "Language3": ""
-        },
-        "ShouldLog": true
-    }
-]
-    
-};
+
 var initialData = {
     "Nodes": [
-        
         {
-            "Guid": "b5a84849-3553-4d52-9cdf-175bd1d21348",
-            "NodeType": 1,
+            "Guid": "b5a84849-3553-4d52-9cdf-175bd1d21348",//id
             "Style": "left: 0px; top: 0px;",
             "Name": {
                 "Chinese": "分支开始8",
@@ -126,7 +10,6 @@ var initialData = {
         },
         {
             "Guid": "b5a84849-3553-4d52-9cdf-175bd1d213148",
-            "NodeType": 2,
             "Style": "left: 110px; top: 110px;",
             "Name": {
                 "Chinese": "分支开始8",
